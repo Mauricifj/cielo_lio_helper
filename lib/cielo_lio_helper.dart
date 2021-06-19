0e3e20700adc42d17bf39584b57f6ee1676ec0dd
Maurici Ferreira Junior <m.f.j@hotmail.com>
2021-06-18 22:07:53 -0300

    Added dartdoc comments

--- lib/cielo_lio_helper.dart
+++ lib/cielo_lio_helper.dart
@@ -21,39 +21,47 @@ class CieloLioHelper {
   static PaymentService _paymentService;
   static CancelService _cancelService;
 
+  /// Sets host and schemes for print, checkout and cancel responses from Lio
   static init({String host, SchemeAggregate schemes}) {
     _printer = PrinterService(schemes.printResponseScheme, host, _channel);
     _paymentService = PaymentService(schemes.paymentResponseScheme, host, _channel);
     _cancelService = CancelService(schemes.reversalResponseScheme, host, _channel);
   }
 
+  /// Enqueue text and its style but does not print
   static enqueue(String text, PrintAlignment alignment, int size, int typeface) {
     _printer.enqueue(text, alignment, size, typeface);
   }
 
+  /// Print all texts enqueued by [CieloLioHelper.enqueue()] and execute [callback] when it's done
   static printQueue(Function(LioResponse response) callback) {
     _printer.print(callback);
   }
 
+  /// Returns the establishment code from InfoManager of Lio
   static Future<String> get ec async {
     final String ec = await _channel.invokeMethod('getEC');
     return ec;
   }
 
+  /// Returns the logic number from InfoManager of Lio
   static Future<String> get logicNumber async {
     final String logicNumber = await _channel.invokeMethod('getLogicNumber');
     return logicNumber;
   }
 
+  /// Returns the battery level or `-1` if something goes wrong
   static Future<double> get batteryLevel async {
     final double batteryLevel = await _channel.invokeMethod('getBatteryLevel');
     return batteryLevel;
   }
 
+  /// Sends a [CheckoutRequest] to Lio and waits until the payment is finished or canceled to execute [callback]
   static checkout(CheckoutRequest request, Function(PaymentResponse response) callback) {
     _paymentService.checkout(request, callback);
   }
 
+  /// Sends a [CancelRequest] to Lio and waits until the cancelment is finished or canceled to execute [callback]
   static cancelPayment(CancelRequest request, Function(PaymentResponse response) callback) {
     _cancelService.cancelPayment(request, callback);
   }
